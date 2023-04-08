a2d4d02e58af68de567b0d66d91aea79d1fc91c7
Ivan Sierra <sierraivan93@gmail.com>
2023-04-08 19:35:12 -0300

    Capa de controlador y router agreagadas.

--- Desafio15_DividrEnCapas/server.js
+++ Desafio15_DividrEnCapas/server.js
@@ -8,12 +8,10 @@ import session from "express-session";
 import compression from "compression";
 import { MONGO_OPTIONS } from "./src/config/mongoOptions.js";
 import { DOT_ENV } from "./src/Dot_Env_Input.js";
-import * as Faker from "./src/Faker.js";
+import Controller from "./src/Controller/controller.js";
 import * as Logger from "./src/Logger.js";
 import * as Passport from "./src/Passport.js";
-import * as Multer from "./src/Multer.js";
-import * as Routes from "./src/Routes.js";
-import * as db from "./src/DBs.js";
+import APIRouter from "./src/Router/router.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -44,6 +42,8 @@ if (DOT_ENV.MODE === "CLUSTER" && cluster.isPrimary) {
   const app = express();
   const httpServer = new Server(app);
   const io = new IOServer(httpServer);
+
+
   // App Uses
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
@@ -70,95 +70,21 @@ if (DOT_ENV.MODE === "CLUSTER" && cluster.isPrimary) {
   );
   app.use(passport.session());
 
-  // Passport
-  passport.use("login", Passport.Login);
-  passport.use("signin", Passport.Signin);
-  passport.serializeUser(Passport.Serializar);
-  passport.deserializeUser(Passport.Deserializar);
+// Passport
+passport.use("login", Passport.Login);
+passport.use("signin", Passport.Signin);
+passport.serializeUser(Passport.Serializar);
+passport.deserializeUser(Passport.Deserializar);
 
-  // App Requests // Router
-  // login
-  app.get("/login", Routes.getLogin);
-  app.post(
-    "/login",
-    passport.authenticate("login", {
-      failureRedirect: "/failLogin",
-      successRedirect: "/",
-    })
-  );
-  app.get("/failLogin", Routes.getFailLogin);
-  // logout
-  app.get("/logout", checkAuthentication, Routes.getLogout);
-  //signin
-  app.get("/signin", Routes.getSignin);
-  app.post(
-    "/signin",
-    Multer.upload.single("avatar"),
-    passport.authenticate("signin", {
-      failureRedirect: "/failSignin",
-      successRedirect: "/",
-    })
-  );
-  app.get("/failSignin", Routes.getFailSignin);
-  // "/"
-  app.get("/", checkAuthentication, Routes.getIndex);
-  // productos
-  app.get("/productos/:filtros?", checkAuthentication, Routes.getProductos);
-  app.post("/productos", Routes.postProductos, Routes.postProductos);
-  // carrito
-  app.get("/carrito", checkAuthentication, Routes.getCarrito);
-  app.get(
-    "/addCarritoProd/:email/:prodId",
-    checkAuthentication,
-    Routes.getAddCarritoProd
-  );
-  app.get(
-    "/deleteCarritoProd/:email/:prodId",
-    checkAuthentication,
-    Routes.getDeleteCarritoProd
-  );
-  // perfil
-  app.get("/perfil", checkAuthentication, Routes.getPerfil);
-  // avatarChange
-  app.post(
-    "/avatarChange",
-    checkAuthentication,
-    Multer.upload.single("newAvatar"),
-    Routes.FileCheck,
-    Routes.postAvatarChange
-  );
-  // pedidoCarrito
-  app.get(
-    "/pedidoCarrito/:email/:productosId",
-    checkAuthentication,
-    Routes.getPedidoCarrito
-  );
-  // Server ON
-  httpServer.listen(PORT, () => {
-    Logger.logConsola.info(
-      `Server iniciado desde el PORT ${PORT} en modo ${DOT_ENV.MODE}`
-    );
-  });
+// App Router
+app.use("/", APIRouter);
 
-  // Websocket io
+// Server ON
+httpServer.listen(PORT, () => {
+  Logger.logConsola.info(`Server iniciado desde el PORT ${PORT} en modo ${DOT_ENV.MODE}`);
+});
 
-  io.on("connection", async (socket) => {
-    socket.on("addMocks", async (data) => {
-      const prodMocks = Faker.getProdMocks();
+// Websocket io
+io.on("connection", Controller.websocket);
 
-      await db.Products.saveMany(prodMocks);
-    });
-
-    socket.on("deleteAllProductos", async (data) => {
-      await db.Products.deleteAll();
-    });
-  });
-
-  function checkAuthentication(req, res, next) {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.redirect("/login");
-    }
-  };
 };
